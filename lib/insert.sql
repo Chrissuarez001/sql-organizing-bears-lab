e98e0007a210f76293f14698ea4690c2fac434d8
Christopher Suarez <chrissuarez001@gmail.com>
2023-03-13 10:14:39 -0400

    commit tree f36546846405b7dc626955812e57d886d4c70879

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Mr. Chocolate " , 1 , 'M' , "Brown" , "Angry" , 1);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Rowdy", 2 , 'M', "Black" , "Nice" , 0);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Tabitha" , 3 , 'F', "White", "Calm" , 1);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Sergeant Brown" , 4 , 'M', "Gray" , "Funny" , 1);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Melissa", 5 , 'F', "Beige", "Quiet" , 0);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Grinch", 6, 'M', "Green", "Mean", 1);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES ("Wendy", 7, 'F', "Pink", "Moody", 1);
+INSERT INTO bears (name, age , sex, color, temperament, alive) VALUES( NULL, 8, 'M',"Blue",  "Aggressive", 0);
